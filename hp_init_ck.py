1832d3a8b83f7a2e496437394b10548ccd39e916
chibak <uha821ap@gmail.com>
2017-01-23 17:34:42 +0900

    JSONパース追加

--- hp_init_ck.py
+++ hp_init_ck.py
@@ -1,33 +1,40 @@
 import sys,json
 template='temple.json'
 
-tf=open(template,'r')
-file = sys.argv[1]
-print "# Chek FILE=", file
-f = open(file, 'r')
-dataJson = json.load(f)
+def doCheck():
+    try:
+        tf=open(template,'r')
 
-templeJson= json.load(tf)
+        file = sys.argv[1]
+        print "# Chek FILE=", file
+        f = open(file, 'r')
+        dataJson = json.load(f)
+    except:
+        print "File Open Error!"
+        exit(-1)
+    templeJson= json.load(tf)
 
-print json.dumps(templeJson)
+    print json.dumps(templeJson)
 
-keyList = dataJson.keys()
-keyList.sort()
-for key in keyList:
-    if key != "user_default":
-        continue
-    else:
-        groupDict = dataJson[key]
-        print groupDict
-        for tkey  in templeJson:
-            tval=templeJson[tkey]
-            if tval != groupDict.get(tkey):
-                print "NG ",tkey,"=",groupDict.get(tkey)
+    keyList = dataJson.keys()
+    keyList.sort()
+    for key in keyList:
+        if key != "user_default":
+            continue
+        else:
+            groupDict = dataJson[key]
+            print groupDict
+            for tkey  in templeJson:
+                tval=templeJson[tkey]
+                if tval != groupDict.get(tkey):
+                    print "NG ",tkey,"=",groupDict.get(tkey)
 
 
         '''
-       '''
 
+       '''
+if __name__ == '__main__':
+    doCheck()
 
 
 #for ckKey , ckValue in groupDict.iteritems():
